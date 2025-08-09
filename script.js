439bb0dc26eb1875ab39a10ec6937b9670a322c0
pblo <pdiazarenas541@gmail.com>
2025-08-09 13:30:32 +0100

    FEAT/ html strcutre + DOM selections js

---
+++ script.js
@@ -0,0 +1,6 @@
+
+  const apiURL = "https://mindicador.cl/api";
+  const inputCLP = document.getElementById("input-clp")
+  const currencySelector = document.getElementById("curr-sel")
+  const btnCalcular = document.getElementById("btn-calcular")
+  const resultDiv = document.getElementById("result")
